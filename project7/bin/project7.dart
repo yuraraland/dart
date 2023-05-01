de6cd68c2a4b52ca4f3f0f66e277a422916d91ee
yuraraland <yuraraland00@gmail.com>
2023-05-01 17:14:04 +0900

    day3 - try, catch

---
+++ project7/bin/project7.dart
@@ -0,0 +1,33 @@
+import 'package:project7/project7.dart' as project7;
+
+void main(List<String> arguments) {
+  //try, catch
+
+  try{
+    //에러가 없을 때 실행하는 로직
+
+    final String name = '손유라';
+    print(name);
+
+  }catch(e){
+    //에러가 있을 때 실행하는 로직
+
+    print(e);
+
+  }
+
+
+  //강제 에러 발생시키기
+  try{
+
+    final String animal = '강아지';
+    throw Exception('이름이 잘못 되었습니다.');
+    print(animal);
+
+  }catch(e){
+
+    print(e);
+
+  }
+
+}
---
+++ project7/test/project7_test.dart
@@ -0,0 +1,8 @@
+import 'package:project7/project7.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
